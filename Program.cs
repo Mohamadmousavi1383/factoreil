dd3c6046926de77c1af5a0a6bfc239605175f3b0
mohammad <148981551+Mohamadmousavi1383@users.noreply.github.com>
2023-11-01 02:14:16 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// mohammad mousavi factorei!
+using System.Diagnostics.Metrics;
+
+namespace consoleApplication1
+{
+    class proggram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter num:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int f = fact(num);
+            Console.WriteLine("factoreil is :{0}", f);
+
+        }
+        static int fact(int num)
+        {
+            if (num == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return num * fact(num - 1);
+            }
+        }
+    }
+}
